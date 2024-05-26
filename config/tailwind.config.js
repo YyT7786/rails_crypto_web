6f1f43e10a29355670eb3e0848e5ab07f27506c4
YyT7786 <yanting7786@gmail.com>
2024-05-26 15:57:21 +0800

    Add tailwind style

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: {
+          50: '#ecfae5',
+          100: '#d0f1c0',
+          200: '#afe695',
+          300: '#8cdc68',
+          400: '#6dd440',
+          500: '#4bcc00',
+          600: '#38bc00',
+          700: '#0ea700',
+          800: '#009300',
+          900: '#007100',
+        }
+      },
+      screens: {
+        '2lg': '1200px',
+      },
     },
   },
   plugins: [
